a72124e80ca9b2d9c91d9cb542832581ecf5ce21
Pablo Garcia <pablo.garcia366@gmail.com>
2022-08-27 11:10:06 -0300

    feat: read configs from environment variable

--- cmd/server/configs/configs.go
+++ cmd/server/configs/configs.go
@@ -1,15 +1,64 @@
 package configs
 
+import (
+	"errors"
+	"fmt"
+	"os"
+	"path/filepath"
+	"runtime"
+
+	"github.com/spf13/viper"
+)
+
 type Configs struct {
 	Port string
 	Host string
 }
 
+func init() {
+	loadConfiguration()
+}
+
+func loadConfiguration() {
+	configName := "default"
+	if _, err := os.Stat("config.yml"); err == nil {
+		configName = "config"
+	} else if !errors.Is(err, os.ErrNotExist) {
+		fmt.Print(err)
+		panic("Unexpected error trying read config file")
+	}
+	viper.SetConfigName(configName)
+	viper.SetConfigType("yaml")
+
+	viper.AddConfigPath(".")
+
+	path := getCallerPath()
+	viper.AddConfigPath(path)
+}
+
+func getCallerPath() string {
+	_, b, _, ok := runtime.Caller(0)
+	if !ok {
+		panic("Error getting the configuration path")
+	}
+	return filepath.Dir(b)
+}
+
 func MakeConfigs() *Configs {
 	// we could trigger something to read envs or command line arguments here
 	// so this could be dynamic
+
 	return &Configs{
-		Port: "8080",
-		Host: "localhost",
+		Port: readFromFile("server.addr.port"),
+		Host: readFromFile("server.addr.host"),
 	}
+}
+
+func readFromFile(key string) string {
+	if err := viper.ReadInConfig(); err != nil {
+		fmt.Println(err)
+		panic("Error reading config file")
+	}
+
+	return viper.GetString(key)
 }
--- cmd/server/configs/configs_test.go
+++ cmd/server/configs/configs_test.go
@@ -1,6 +1,7 @@
 package configs
 
 import (
+	"os"
 	"testing"
 )
 
@@ -13,3 +14,61 @@ func TestMakeConfigs(t *testing.T) {
 		t.Fatal("configs must have non zero value for Port")
 	}
 }
+
+func TestGetConfigFromFile(t *testing.T) {
+	testsCases := map[string]struct {
+		key      string
+		expected string
+	}{
+		"Gets port": {"server.addr.port", "3000"},
+		"Gets host": {"server.addr.host", "localhost"},
+	}
+	for name, tc := range testsCases {
+		tf := func(t *testing.T) {
+			want := tc.expected
+
+			got := readFromFile(tc.key)
+
+			if got != want {
+				t.Errorf("want '%s', got '%s'", want, got)
+			}
+		}
+		t.Run(name, tf)
+	}
+
+}
+
+
+func TestSetAConfigFile(t *testing.T) {
+	content := `
+server:
+  addr:
+    port: 1111
+    host: test
+`
+	f, err := os.Create("./config.yml")
+	if err != nil {
+		panic("Error creating test config file")
+	}
+    
+	_, err = f.WriteString(content)
+    if err != nil {
+        f.Close()
+        return
+    }
+	cleanup := func () {
+		os.Remove("./config.yml")
+	}
+	defer cleanup()
+	f.Close()
+
+	want := "1111"
+	loadConfiguration()
+	got := MakeConfigs().Port
+
+	if want != got {
+		t.Errorf("want '%s', got '%s'", want, got )
+	}
+
+} 
+
